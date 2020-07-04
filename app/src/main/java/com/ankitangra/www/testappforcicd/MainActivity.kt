ef92a1366952ab1fd520c04cf7c1460f80130310
Angra, Ankit <ankit.angra@bellmedia.ca>
2020-07-04 17:58:10 -0400

    Added App Center credentials

--- app/src/main/java/com/ankitangra/www/testappforcicd/MainActivity.kt
+++ app/src/main/java/com/ankitangra/www/testappforcicd/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.ankitangra.www.testappforcicd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "0d6d3abd-e96a-4c72-a4c6-580ec6f3cf33",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
